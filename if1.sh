784db9b9fec80314ea5cb0d46c321cdc0779a1a8
pravallika1279 <dhrithishravika@gmail.com>
2020-11-27 20:58:47 +0530

    first commit

---
+++ if1.sh
@@ -0,0 +1,10 @@
+:wq#!/bin/bash
+echo "Enter value between 1 and 5: "
+read VALUE
+
+if [ "$VALUE" -eq 1 ] || [ "$VALUE" -eq "3" ] || [ "$VALUE" -eq "5" ]; then
+	echo "You entered a ODD value of $VALUE"
+else
+	echo "You entered a even  value of $VALUE"
+fi
+
